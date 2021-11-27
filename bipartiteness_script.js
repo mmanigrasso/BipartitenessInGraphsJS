e05da68c31aed86c6978537c3823c6a62271d803
mmanigrasso <m.manigrasso@outlook.com>
2021-11-27 18:43:01 +0100

    Initial commit

---
+++ bipartiteness_script.js
@@ -0,0 +1,10 @@
+/**
+ * @file Excercise16_bipartiteness
+ * @author Marco Manigrasso (mamanigrasso@edu.aau.at)
+ * @brief This programm will check the bipartiteness of a given graph by coloring the vertices with two different colors.
+ * @version 1.0
+ * @date 2021-11-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
